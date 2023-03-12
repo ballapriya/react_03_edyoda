9b79a0063e06cdae8b715445d0bf3b8459646cd4
ballapriya <126747477+ballapriya@users.noreply.github.com>
2023-03-12 14:22:52 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import MoviesApp from "./Component/Moviesapp";
+
+function App() {
+  return (
+    <div className="App">
+     <MoviesApp/>
+    </div>
+  );
+}
+
+export default App;
